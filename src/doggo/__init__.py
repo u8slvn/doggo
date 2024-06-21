51288024691a055a75a4b86b6a749dd6df88a847
u8slvn <u8slvn@gmail.com>
2024-06-21 10:16:49 +0200

    build: add dependencies

---
+++ src/doggo/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from importlib import metadata
+
+
+__app_name__ = "doggo"
+__version__ = metadata.version(__app_name__)
---
+++ src/doggo/__main__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+
+def run() -> None:
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    run()
