81d43c34dd50ad43d6b42bff0a83b743a0d6b1b2
u8slvn <u8slvn@gmail.com>
2024-07-08 12:53:23 +0200

    feat: add all the states based on sprite sheet

--- src/doggo/dna.py
+++ src/doggo/dna.py
@@ -17,10 +17,18 @@ SPRITE_DIRECTION = Direction.LEFT  # Default direction of the sprite
 # Number of sprites per state in the sprite sheet and row index
 SPRITE_CONF_PER_STATE = {
     StateID.IDLE: (6, 0),
+    StateID.IDLE_AND_BARK: (8, 0),
     StateID.WALK: (8, 4),
-    StateID.RUN: (8, 3),
-    StateID.SLEEP: (4, 8),
+    StateID.WALK_AND_BARK: (8, 6),
     StateID.SIT: (6, 1),
+    StateID.SIT_AND_BARK: (8, 1),
+    StateID.LIE_DOWN: (6, 2),
+    StateID.LIE_DOWN_AND_BARK: (8, 2),
+    StateID.RUN: (8, 3),
+    StateID.RUN_AND_BARK: (8, 5),
+    StateID.STAND: (6, 7),
+    StateID.STAND_AND_BARK: (8, 7),
+    StateID.SLEEP: (4, 8),
 }
 
 
