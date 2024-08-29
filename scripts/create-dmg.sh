d72135792564121d52e8053e480498744d20921a
u8slvn <u8slvn@gmail.com>
2024-08-29 11:17:24 +0200

    build: update dmg build script

--- scripts/create-dmg.sh
+++ scripts/create-dmg.sh
@@ -2,6 +2,13 @@
 
 create-dmg \
     --volname "Doggo" \
+    --window-pos 200 200 \
+    --window-size 460 350 \
+    --icon "Doggo.app" 130 60 \
+    --icon-size 64 \
     --hide-extension "Doggo.app" \
+    --app-drop-link 325 60 \
+    --format UDBZ \
+    --no-internet-enable \
     "Doggo.dmg" \
     "./dist/Doggo.app"
